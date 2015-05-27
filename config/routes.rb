e62a2edf64fe257a3cdbc48312b80aa3b16c0ee9
Ivan Zarea <zarea.ion@gmail.com>
2015-05-27 19:49:22 +0300

    Tip 11 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => '/attachinary' 
   devise_for :users
   resources :posts
+  authenticated :user do
+    root to: "posts#index", as: "authenticated_user"
+  end
   root to: "pages#landing"  
   get "features" => "pages#features"
   get "landing" => "pages#landing"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
