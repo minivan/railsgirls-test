1ebd91ad5f2ccc672b6e05a78cb21936d18d639a
Andrei Istratii <andrei.istratii@gmail.com>
2015-05-30 00:34:03 +0300

    Add Facebook Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  mount Attachinary::Engine => '/attachinary' 
-  devise_for :users
+  mount Attachinary::Engine => '/attachinary'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :posts
   authenticated :user do
     root to: "posts#index", as: "authenticated_user"
   end
-  root to: "pages#landing"  
+  root to: "pages#landing"
   get "features" => "pages#features"
   get "landing" => "pages#landing"
 
